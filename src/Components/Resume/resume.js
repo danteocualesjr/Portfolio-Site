c7eb86d341a9e4f27e63890a93453942ae00a2a4
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-09-28 13:15:59 +0800

    Edited Resume component.

--- src/Components/Resume/resume.js
+++ src/Components/Resume/resume.js
@@ -45,8 +45,8 @@ class Resume extends Component {
                 <div className="nine columns main-col">
                     <div className="row item">
                     <div className="twelve columns">
-                        <h3>Awesome Design Studio</h3>
-                        <p className="info">Senior UX Designer <span>•</span> <em className="date">March 2010 - Present</em></p>
+                        <h3>Lambda School</h3>
+                        <p className="info">Section Lead and Project Manager <span>•</span> <em className="date">September 2018 - Present</em></p>
                         <p>
                         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
                         Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
@@ -57,13 +57,13 @@ class Resume extends Component {
                     </div> {/* item end */}
                     <div className="row item">
                     <div className="twelve columns">
-                        <h3>Super Cool Studio</h3>
-                        <p className="info">UX Designer <span>•</span> <em className="date">March 2007 - February 2010</em></p>
+                        <h3>CebuDAC Transport and Van Services</h3>
+                        <p className="info">Operations Manager <span>•</span> <em className="date">January 2002 - April 2018</em></p>
                         <p>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
-                        nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan
-                        ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+                        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.
                         </p>
                     </div>
                     </div> {/* item end */}
