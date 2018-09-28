97c5759878b510894631be95f205c2dd08479677
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-09-29 06:55:44 +0800

    Added an item under the Education section in the Resume component.

--- src/Components/Resume/resume.js
+++ src/Components/Resume/resume.js
@@ -25,7 +25,19 @@ class Resume extends Component {
                     <div className="row item">
                     <div className="twelve columns">
                         <h3>University of the Visayas</h3>
-                        <p className="info">Bachelor of Arts in Nursing <span>•</span> <em className="date">March 2008</em></p>
+                        <p className="info">Bachelor of Science in Nursing <span>•</span> <em className="date">March 2008</em></p>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+                        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.
+                        </p>
+                    </div>
+                    </div> {/* item end */}
+                    <div className="row item">
+                    <div className="twelve columns">
+                        <h3>Cebu Doctors' University</h3>
+                        <p className="info">Bachelor of Arts in Psychology <span>•</span> <em className="date">March 2008</em></p>
                         <p>
                         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
                         Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
