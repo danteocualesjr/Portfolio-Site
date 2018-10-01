443a2d66fc8e593db9d724e717814943ef6fc560
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-01 22:31:13 +0800

    Edited Testimonials component.

--- src/Components/Testimonials/testimonials.js
+++ src/Components/Testimonials/testimonials.js
@@ -7,12 +7,13 @@ class Testimonials extends Component {
                 <div className="text-container">
                 <div className="row">
                     <div className="two columns header-col">
-                    <h1><span>Client Testimonials</span></h1>
+                    <h1><span>Blog</span></h1>
                     </div>
                     <div className="ten columns flex-container">
                     <div className="flexslider">
-                        <ul className="slides">
-                        <li>
+                        {/* <ul className="slides"> */}
+                            <img alt src="images/blogpost1.jpg" />
+                        {/* <li>
                             <blockquote>
                             <p>Your work is going to fill a large part of your life, and the only way to be truly satisfied is
                                 to do what you believe is great work. And the only way to do great work is to love what you do.
@@ -20,17 +21,9 @@ class Testimonials extends Component {
                             </p>
                             <cite>Steve Jobs</cite>
                             </blockquote>
-                        </li> {/* slide ends */}
-                        <li>
-                            <blockquote>
-                            <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                                Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
-                                nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.
-                            </p>
-                            <cite>Mr. Adobe</cite>
-                            </blockquote>
-                        </li> {/* slide ends */}
-                        </ul>
+                        </li>  */}
+                        {/* slide ends */}
+                        {/* </ul> */}
                     </div> {/* div.flexslider ends */}
                     </div> {/* div.flex-container ends */}
                 </div> {/* row ends */}
