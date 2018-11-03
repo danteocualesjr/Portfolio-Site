b4c54e52eacc79030700e146340da62ecfde5bad
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-03 23:20:35 +0800

    Edited about.js.

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -9,8 +9,8 @@ class About extends Component {
                     <img className="profile-pic" src="images/profilephoto2.jpg" />
                 </div>
                 <div className="nine columns main-col">
-                    <h2>About Me</h2>
-                    <p>I'm currently the Section Lead for, and one of the Project Managers of, Lambda School's "Lambda Next", a post-graduate program designed to help its alumni land Software Engineering jobs quickly. I'm super excited, grateful, and honored to be part of this awesome project.</p>
+                    <h2>About</h2>
+                    <p>I'm currently the Section Lead, and one of the Project Managers, of Lambda School's "Lambda Next", a post-graduate program designed to help its alumni land Software Engineering jobs quickly. I'm super excited, grateful, and honored to be part of this awesome project.</p>
                     <p>I'm also a proud student of the same school. We just finished learning Full Stack Web Development and Computer Science and it was a blast.</p>
                     <p>I'm also a writer. In my spare time, I write short stories, opinion pieces, and articles on programming and Computer Science. My short fiction and opinion pieces have appeared in The Philippines Graphic and The Philippine Daily Inquirer, while my articles have been published in Hacker Noon and Medium.</p>
                     <p>Feel free to reach out to me if you need anything!</p>
