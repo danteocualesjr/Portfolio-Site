3856a192fff745fc771a1e5ee975631f85821ca7
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-14 08:22:04 +0800

    Added links behind the social media icons in Header component.

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -25,12 +25,11 @@ class Header extends Component {
                 <h3><span>Section Lead</span> and <span>Project Manager</span> at Lambda School | <span>{resumeData.role}</span> | <span>Software Engineer</span> | <span>Writer</span> @ Hacker Noon</h3>
                 <hr/>
                 <ul className="social">
-                    <li><a href="#"><i className="fa fa-facebook" /></a></li>
-                    <li><a href="#"><i className="fa fa-twitter" /></a></li>
-                    <li><a href="#"><i className="fa fa-google-plus" /></a></li>
-                    <li><a href="#"><i className="fa fa-linkedin" /></a></li>
-                    <li><a href="#"><i className="fa fa-instagram" /></a></li>
-                    <li><a href="#"><i className="fa fa-dribbble" /></a></li>
+                    <li><a href="https://www.facebook.com/dante.cuales.jr"><i className="fa fa-facebook" /></a></li>
+                    <li><a href="https://twitter.com/dantecualesjr"><i className="fa fa-twitter" /></a></li>
+                    {/* <li><a href="#"><i className="fa fa-google-plus" /></a></li> */}
+                    <li><a href="https://www.linkedin.com/in/danteocualesjr/"><i className="fa fa-linkedin" /></a></li>
+                    <li><a href="https://www.instagram.com/danteocualesjr/"><i className="fa fa-instagram" /></a></li>
                     <li><a href="#"><i className="fa fa-skype" /></a></li>
                 </ul>
                 </div>
