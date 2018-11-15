20621bd0044da56e51c9c3a6b6e4bc05a59cbca8
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-15 20:33:09 +0800

    Commented out links to Google Plus and Dribble in Footer.

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -9,10 +9,10 @@ class Footer extends Component {
                         <ul className="social-links">
                         <li><a href="#"><i className="fa fa-facebook" /></a></li>
                         <li><a href="#"><i className="fa fa-twitter" /></a></li>
-                        <li><a href="#"><i className="fa fa-google-plus" /></a></li>
+                        {/* <li><a href="#"><i className="fa fa-google-plus" /></a></li> */}
                         <li><a href="#"><i className="fa fa-linkedin" /></a></li>
                         <li><a href="#"><i className="fa fa-instagram" /></a></li>
-                        <li><a href="#"><i className="fa fa-dribbble" /></a></li>
+                        {/* <li><a href="#"><i className="fa fa-dribbble" /></a></li> */}
                         <li><a href="#"><i className="fa fa-skype" /></a></li>
                         </ul>
                     </div>
