28c1e848d295f3b69014ce227d49ba805aa35225
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-15 20:29:35 +0800

    Added comment in About component for resume.

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -28,7 +28,8 @@ class About extends Component {
                     </div>
                     <div className="columns download">
                         <p>
-                        <a href="#" className="button"><i className="fa fa-download" />Download My Resume</a>
+                        {/* TODO: Look up code for downloading resume. */}
+                        <a href="#" className="button"><i className="fa fa-download"/>Download My Resume</a>
                         </p>
                     </div>
                     </div> {/* end row */}
