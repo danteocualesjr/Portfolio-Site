b94dda9709260ace0ce735a7f18d528d3076f8ca
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-17 23:57:42 +0800

    Commented out line 18 in header.js.

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -15,7 +15,7 @@ class Header extends Component {
                 <li><a className="smoothscroll" href="#about">About</a></li>
                 <li><a className="smoothscroll" href="#resume">Resume</a></li>
                 <li><a className="smoothscroll" href="#portfolio">Portfolio</a></li>
-                <li><a className="smoothscroll" href="#blog">Blog</a></li>
+                {/* <li><a className="smoothscroll" href="#blog">Blog</a></li> */}
                 <li><a className="smoothscroll" href="#contact">Contact</a></li>
                 </ul> {/* end #nav */}
             </nav> {/* end #nav-wrap */}
